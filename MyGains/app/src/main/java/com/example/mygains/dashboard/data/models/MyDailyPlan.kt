6f1c0ecc8794109972239bf07052169223605a97
adrian sanz <adrian.sanzllamas@gmail.com>
2025-01-22 16:46:27 +0100

    residual imports

--- MyGains/app/src/main/java/com/example/mygains/dashboard/data/models/MyDailyPlan.kt
+++ MyGains/app/src/main/java/com/example/mygains/dashboard/data/models/MyDailyPlan.kt
@@ -1,3 +1,3 @@
-package com.example.mygains.dashboard.data
+package com.example.mygains.dashboard.data.models
 
 data class MyDailyPlan(var date:String="", var exercisesList:MutableList<String> = mutableListOf())
--- MyGains/app/src/main/java/com/example/mygains/dashboard/domain/usecases/DashBoardUseCase.kt
+++ MyGains/app/src/main/java/com/example/mygains/dashboard/domain/usecases/DashBoardUseCase.kt
@@ -1,4 +1,4 @@
-package com.example.mygains.dashboard.domain
+package com.example.mygains.dashboard.domain.usecases
 
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
